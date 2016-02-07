44abd6e85e14423bf5d413f9cead8249a3c5a4ad
Abhijit <aykaranj@indiana.edu>
2016-02-06 19:25:24 -0500

    Changing Package structure

---
+++ WPST_Web_App/WPST_Web_App/src/main/java/com/cigital/wpst/bean/WpstResponseBean.java
@@ -0,0 +1,18 @@
+package com.cigital.wpst.bean;
+
+import java.util.List;
+
+public class WpstResponseBean {
+
+	private List<String> openPorts;
+
+	public List<String> getOpenPorts() {
+		return openPorts;
+	}
+
+	public void setOpenPorts(List<String> openPorts) {
+		this.openPorts = openPorts;
+	}
+	
+	
+}
--- WPST_Web_App/WPST_Web_App/src/main/java/com/cigital/wpst/rest/ws/WpstWebServiceController.java
+++ WPST_Web_App/WPST_Web_App/src/main/java/com/cigital/wpst/rest/ws/WpstWebServiceController.java
@@ -1,4 +1,4 @@
-package com.cigital.wpst.webservice;
+package com.cigital.wpst.rest.ws;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
