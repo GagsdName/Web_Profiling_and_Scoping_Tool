09f66a2c98bb13a00e9fd9cc9ae55ab670a8b602
Abhijit <aykaranj@indiana.edu>
2016-02-06 19:13:48 -0500

    http://tintin.cs.indiana.edu:8091/browse/WPSN-8

---
+++ WPST_Web_App/WPST_Web_App/src/main/java/com/cigital/wpst/webservice/WpstWebServiceController.java
@@ -0,0 +1,11 @@
+package com.cigital.wpst.webservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("profiling_service")
+public class WpstWebServiceController {
+
+	
+}
