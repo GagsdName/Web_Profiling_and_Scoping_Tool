eff15379988fe7d2d392f2a2549839837c191f9f
Pranav Pande <pmpande@iu.edu>
2016-04-06 01:40:52 -0400

    created json object

--- ProfilingChromeExtension/WebProfiling/js/custom/cookieInfo.js
+++ ProfilingChromeExtension/WebProfiling/js/custom/cookieInfo.js
@@ -4,20 +4,23 @@ function cookieInfo() {
 
 function getCookieInfo(){
     chrome.tabs.query({"status":"complete","windowId":chrome.windows.WINDOW_ID_CURRENT,"active":true}, function(tab){
-            //console.log(JSON.stringify(tab));
 			var x = document.getElementById("login").checked;
-			console.log(x);
+			var cookieInfoData = '{"Login": '+ x + ',';
             chrome.cookies.getAll({"url":tab[0].url},function(cookie){
-                console.log(cookie.length);
-                allCookieInfo = "";
-                for(i=0;i<cookie.length;i++){
-                    console.log(JSON.stringify(cookie[i]));
-                    allCookieInfo = allCookieInfo + JSON.stringify(cookie[i]);
-                }
-                localStorage.currentCookieInfo = allCookieInfo;
+				cookieInfoData = cookieInfoData + '"Cookie Length": '+ cookie.length + ',';
+				allCookieInfo = "";
+				if(cookie.length > 0){
+					allCookieInfo = JSON.stringify(cookie[0]);
+					for(i=1;i<cookie.length;i++){
+						allCookieInfo = allCookieInfo + ',' + JSON.stringify(cookie[i]);
+					}
+				}
+				cookieInfoData = cookieInfoData + '"Cookies": [' + allCookieInfo + ']}';
+				var cookieObj = JSON.parse(cookieInfoData);
+                localStorage.currentCookieInfo = cookieObj;
+				console.log(cookieObj);
             });
     });
-
 }
   
 window.addEventListener("load",cookieInfo);
