28b6ef0ced69514e85e531cf9f7379d8c0df6784
singhgag <singhgag@indiana.edu>
2016-04-15 03:05:44 -0400

    Bootstrap tour on crawler configuration
    Added bootstrap tour on crawler configuration.

--- ProfilingChromeExtension/WebProfiling/js/custom/popupOnload.js
+++ ProfilingChromeExtension/WebProfiling/js/custom/popupOnload.js
@@ -13,30 +13,43 @@ function tour_formOnload(){
   {
     element: "#start",
     title: "Start Page",
+	prev: -1,
+	next: 1,
     content: "Start crawling from this page"
   },
    {
     element: "#regex",
     title: "Regular Expression",
+	prev: 0,
+	next: 2,
     content: "URL Regex you want to limit crawling to"
   },
   {
     element: "#plusone",
     title: "Beyond Restrictions",
+	prev: 1,
+	next: 3,
     content: "Follow links beyond restriction"
   },
   {
     element: "#inline",
     title: "Inline",
+	prev: 2,
+	next: 4,
     content: "To crawl inline content"
   },
    {
     element: "#scripts",
     title: "Scripts",
+	prev: 3,
+	next: 5,
     content: "Crawl any script tags on the page"
-  }, {
-    element: "#arguments",
+  }, 
+  {
+    element: "#start",
     title: "Avoid Arguments",
+	prev: -1,
+	next: 1,
     content: "Avoid Links with Arguments"
   }
 ]});
