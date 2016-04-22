314b5741a3a952124e45ea604daf7bb239b744cc
Pranav Pande <pmpande@iu.edu>
2016-04-22 12:27:04 -0400

    moved cookies from active to passive

--- ProfilingChromeExtension/WebProfiling/js/custom/cookieInfo.js
+++ ProfilingChromeExtension/WebProfiling/js/custom/cookieInfo.js
@@ -1,7 +1,7 @@
 var cookieInfoJSON = {};
 var cookies = [];
-function cookieInfo() {
-	var cookieElement = document.getElementById("getCookieInfo");
+/*function cookieInfo() {
+	//var cookieElement = document.getElementById("getCookieInfo");
 	if(cookieElement !== null){
 		document.getElementById("getCookieInfo").addEventListener("click",getCookieInfo);	
 	}else{
@@ -10,21 +10,18 @@ function cookieInfo() {
 	}
 }
 
-
-
 function clearLocalStorage(){
 	localStorage.clear();
 	getCookieInfo();
-}
+}*/
 
 function getCookieInfo(){
 	var element = {};
-	if(localStorage.getItem("cookieInfo") !== null){
+    if(localStorage.getItem('cookieInfo') !== null){
+		console.log("Session storage defined");
 		element.login = true;
-		cookies.push(localStorage.getItem("cookieInfo"));
-		cookieInfoJSON['Cookies before login'] = JSON.parse(localStorage.getItem("cookieInfo"));
-		
 	}else{
+		console.log("Session storage undefined");
 		element.login = false;
 	}
     chrome.tabs.query({"status":"complete","windowId":chrome.windows.WINDOW_ID_CURRENT,"active":true}, function(tab){
@@ -35,21 +32,28 @@ function getCookieInfo(){
 					allCookieInfo.push(cookie[i]);
 				}
 				element.cookies = allCookieInfo;
-				
-				localStorage.setItem("cookieInfo",JSON.stringify(element));
 				if(element.login == true){
+					console.log("Login true");
 					cookies.push(JSON.stringify(element));
 					cookieInfoJSON['Cookies after login'] = element;
 					jsonOutput($(".cookie-json"), cookieInfoJSON);
-					
-				}
-				 	$("#cookieInfo").addClass('hide');
+					$("#cookieInfo").addClass('hide');
 					$("#cookieInfo").html(cookies[0] + "</br></br>" + cookies[1]);
 					$("#cookieInfo").removeClass('hide');
+				}else{
+					console.log("Login false");
+					localStorage.setItem('cookieInfo',JSON.stringify(element));
+					cookies.push(localStorage.getItem("cookieInfo"));
+					cookieInfoJSON['Cookies before login'] = JSON.parse(localStorage.getItem('cookieInfo'));
+					jsonOutput($(".cookie-json"), cookieInfoJSON);
+					$("#cookieInfo").addClass('hide');
+					$("#cookieInfo").html(cookies[0]);
+					$("#cookieInfo").removeClass('hide');
+				}
             });
            
     });
     
 }
   
-window.addEventListener("load",cookieInfo);
+window.addEventListener("load",getCookieInfo);
